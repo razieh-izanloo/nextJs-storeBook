1b5ef60b8e876d8e90744cc51e06c6f7d0aa97fa
razieh i <izanloo@amwajco.net>
2025-07-20 22:23:01 +0330

    feat: add file for constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,10 @@
+export const LANG_COOKIE_DURATION = 60 * 60 * 24 * 15; // 15 days
+
+export const LANG_COOKIE_NAME = "LOCALE";
+
+export const support_langs = ["en", "fa"];
+
+export const INFO_LANGS: { [key: string]: { dir: string } } = {
+  en: { dir: "ltr" },
+  fa: { dir: "rtl" },
+};
