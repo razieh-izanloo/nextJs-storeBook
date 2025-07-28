cf516b73bcb6d62a7d942a27695d1221104c56f5
razieh i <izanloo@amwajco.net>
2025-07-28 17:11:22 +0330

    add label & dir for langs

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -4,7 +4,8 @@ export const LANG_COOKIE_NAME = "LOCALE";
 
 export const support_langs = ["en", "fa"];
 
-export const INFO_LANGS: { [key: string]: { dir: string } } = {
-  en: { dir: "ltr" },
-  fa: { dir: "rtl" },
+export const INFO_LANGS: { [key: string]: { dir: string, label: string } } = {
+  en: { dir: "ltr", label: "English" },
+  fa: { dir: "rtl", label: "فارسی" },
 };
+
